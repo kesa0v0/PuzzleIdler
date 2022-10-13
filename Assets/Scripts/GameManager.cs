08d75bc9752e9aa5eeae6ac47df6be36d4c35888
kesa <kesa0v0@gmail.com>
2022-10-14 01:08:37 +0900

    일단 UI 업데이트 structure는 짜놓았어용

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,90 @@ using UnityEngine.UIElements;
 
 using Cysharp.Threading.Tasks;
 using System.Threading.Tasks;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+    // singletone
+    public static GameManager Instance;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            Configure();
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+        }
+    }
 
+
+    public int preferedFPS = 60;
+
+
+    //TODO: 커스텀 자료형
+    public float currentPoint = 0;
+    public float maxPoint = 1000;
+    public float incPointPerSec = 1;
+    public int currentLevel = 1;
+
+    // Start UpdateCalcPoint()
+    private void Start()
+    {
+        UpdateCalcPoint();
+    }
+    
+    private void Update() {
+        ui_CurrencyLabel.text = currentPoint.ToString() + " / " + maxPoint.ToString();
+        ui_ICPSLabel.text = incPointPerSec.ToString() + " / sec";
+        ui_LevelLabel.text = "Level " + currentLevel.ToString();
+    }
+
+    public async void UpdateCalcPoint()
+    {
+        while (true)
+        {
+            await UniTask.Delay(1000/preferedFPS);
+            currentPoint += CalcPointPerFPS();
+        }
+    }
+
+    private float CalcPointPerFPS()
+    {
+        return incPointPerSec / preferedFPS;
+    }
+    
 
     #region UI elements
+
+
     private VisualElement ui_Root;
 
+    private static Label ui_CurrencyLabel;
+    // Increasing Currency per second
+    private static Label ui_ICPSLabel;
 
-    private static Label ui_ItemDetailHeader;
-    private static Label ui_ItemDetailBody;
-    private bool IsUIReady;
+    private static Label ui_LevelLabel;
+
+    // private bool IsUIReady;
 
 
     private async void Configure()
     {
+        Application.targetFrameRate = preferedFPS;
+
+        // UI
         ui_Root = GetComponentInChildren<UIDocument>().rootVisualElement;
-        VisualElement itemDetails = ui_Root.Q<VisualElement>("ItemDetails");
-        ui_ItemDetailHeader = itemDetails.Q<Label>("Header");
-        ui_ItemDetailBody = itemDetails.Q<Label>("Body");
+        ui_CurrencyLabel = ui_Root.Q<Label>("Currency_Value");
+        ui_ICPSLabel = ui_Root.Q<Label>("IncreasingCurrencyPerSecValue");
+        ui_LevelLabel = ui_Root.Q<Label>("Level_Value");
 
         await UniTask.Yield(PlayerLoopTiming.LastPostLateUpdate);
         
-        IsUIReady = true;
+        // IsUIReady = true;
     }
 
     #endregion
