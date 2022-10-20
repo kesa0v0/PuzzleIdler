2d59275eea8fd7a38ab96112530b17ef088b145d
kesa <kesa0v0@gmail.com>
2022-10-20 21:46:17 +0900

    Position을 Util로 분리

---
+++ Assets/Scripts/Utils/Position.cs
@@ -0,0 +1,42 @@
+
+using System;
+
+[Serializable]
+public struct Position
+{
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public int x;
+    public int y;
+    public static bool operator ==(Position a, Position b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+    public static bool operator !=(Position a, Position b)
+    {
+        return !(a == b);
+    }
+
+    public static Position operator +(Position a, Position b)
+    {
+        return new Position(a.x + b.x, a.y + b.y);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
